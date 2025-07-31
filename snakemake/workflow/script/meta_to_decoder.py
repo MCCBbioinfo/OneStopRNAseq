d4347f0a86d23f3f4d29c076b51502312e678a26
hukai916 <hukai916@gmail.com>
2025-07-31 16:10:44 -0400

    handle NAs in cleanuprnaseq.makemeta.py and meta_to_decoder.py

--- snakemake/workflow/script/meta_to_decoder.py
+++ snakemake/workflow/script/meta_to_decoder.py
@@ -4,13 +4,14 @@ import sys
 def read_table(fname='meta/contrast.de.xlsx'):
     try:
         if fname.endswith(".txt"):
-            df = pd.read_table(fname)
+            df = pd.read_table(fname, keep_default_na=False, na_values=[])
         elif fname.endswith(".csv"):
-            df = pd.read_csv(fname)
+            df = pd.read_csv(fname, keep_default_na=False, na_values=[])
         elif fname.endswith(".xlsx"):
             df = pd.read_excel(fname, engine='openpyxl', keep_default_na=False, na_values=[])
         else:
             sys.exit("fname not xlsx nor txt")
+        df = df.replace("NA", "NA_")
     except:
         sys.exit(">>> Fatal Error: " + fname + " format error, it can't be read correctly." +
                  "\nPlease check if you saved txt file as xlsx or vice versa\n\n")
